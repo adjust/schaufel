dbdfe37855803c7d3b91f807e7fff74709b36b32
Felix Wischke (65278) <felix@adjust.com>
2019-03-11 14:09:27 +0100

    t/queue_test.c: properly fixed queue_test this time

--- t/queue_test.c
+++ t/queue_test.c
@@ -7,9 +7,9 @@ main(void)
     Queue q = queue_init();
     Message msg = message_init();
     char *data = "moep";
-    queue_add(q, data, sizeof(data), 1);
+    queue_add(q, data, strlen(data), 1);
     queue_get(q, msg);
-    pretty_assert(strncmp(data, (char *) message_get_data(msg), 4) == 0);
+    pretty_assert(strncmp(data, (char *) message_get_data(msg), strlen(data)) == 0);
     queue_free(&q);
     message_free(&msg);
     return 0;
