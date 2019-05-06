568fc17ca6e9af52745c079291fca7fea94f8b8e
Felix Wischke (65278) <felix@adjust.com>
2019-05-06 11:40:48 +0200

    utils/scalloc.h: removed comments

--- src/utils/scalloc.h
+++ src/utils/scalloc.h
@@ -1,18 +1,11 @@
 #ifndef _SCHAUFEL_UTILS_SCALLOC_H
 #define _SCHAUFEL_UTILS_SCALLOC_H
 
-#include "logger.h"
-/*
-#define SCALLOC(r, n, s) \
-        if (!((r) = calloc((n),sizeof(s)))) { \
-            logger_log("%s %d: Failed to calloc: %s\n", __FILE__, __LINE__, \
-            strerror(errno)); \
-            abort(); \
-        }
-*/
+#include "utils/logger.h"
 
-void *scalloc(size_t n, size_t l, char *file, size_t line);
+void *scalloc(size_t n, size_t s, char *file, size_t line);
 #define SCALLOC(n, s) scalloc(( n), (s), __FILE__, __LINE__);
+
 #define SFREE(e) \
     free(e); \
     e = NULL;
