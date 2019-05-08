bb7629050dee751f4a5c245a838469d9747521b3
Felix Wischke (65278) <felix@adjust.com>
2019-05-08 13:27:46 +0200

    utils/scalloc.h: cleanup

--- src/utils/scalloc.h
+++ src/utils/scalloc.h
@@ -4,11 +4,12 @@
 #include "utils/logger.h"
 
 void *scalloc(size_t n, size_t s, char *file, size_t line);
-#define SCALLOC(n, s) scalloc(( n), (s), __FILE__, __LINE__);
+#define SCALLOC(n, s) scalloc(( n), (s), __FILE__, __LINE__)
 
 #define SFREE(e) \
-    free(e); \
-    e = NULL;
-
+    do { \
+        free(e); \
+        e = NULL; \
+    } while (0)
 
 #endif
