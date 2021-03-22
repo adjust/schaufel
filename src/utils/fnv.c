6b8063f33ef7c7571874aa8ba15281f4704d790c
Felix Wischke (65278) <felix@adjust.com>
2021-03-22 15:07:09 +0100

    src/utils/fnv.c: general folding

--- src/utils/fnv.c
+++ src/utils/fnv.c
@@ -33,22 +33,44 @@ Fnv32_t fnv32a_str(void *buf, size_t len)
     return _fnv32a((void *) buf, len, FNV_32A_INIT);
 }
 
-Fnv32_t fold_noop(Fnv32_t hval)
+static inline Fnv32_t _fold(uint8_t fold, Fnv32_t hval)
 {
-    return hval;
+    uint32_t mask = (1<<(uint32_t)fold) -1;
+    return(hval>>(32-fold)) ^ (hval & mask);
 }
 
-Fnv32_t fold16(Fnv32_t hval)
-{
-    #define MASK_16 (((uint32_t)1<<16)-1) /* i.e., (u_int32_t)0xffff */
-    return (hval>>16) ^ (hval & MASK_16);
-}
-
-Fnv32_t fold1(Fnv32_t hval)
-{
-    #define MASK_1 (((uint32_t)1<<1)-1) /* i.e., (u_int32_t)0x1 */
-    return (hval>>31) ^ (hval & MASK_1);
-}
+Fnv32_t fold_noop(Fnv32_t hval) {return hval;}
+Fnv32_t fold31(Fnv32_t hval) {return _fold(31,hval);}
+Fnv32_t fold30(Fnv32_t hval) {return _fold(30,hval);}
+Fnv32_t fold29(Fnv32_t hval) {return _fold(29,hval);}
+Fnv32_t fold28(Fnv32_t hval) {return _fold(28,hval);}
+Fnv32_t fold27(Fnv32_t hval) {return _fold(27,hval);}
+Fnv32_t fold26(Fnv32_t hval) {return _fold(26,hval);}
+Fnv32_t fold25(Fnv32_t hval) {return _fold(25,hval);}
+Fnv32_t fold24(Fnv32_t hval) {return _fold(24,hval);}
+Fnv32_t fold23(Fnv32_t hval) {return _fold(23,hval);}
+Fnv32_t fold22(Fnv32_t hval) {return _fold(22,hval);}
+Fnv32_t fold21(Fnv32_t hval) {return _fold(21,hval);}
+Fnv32_t fold20(Fnv32_t hval) {return _fold(20,hval);}
+Fnv32_t fold19(Fnv32_t hval) {return _fold(19,hval);}
+Fnv32_t fold18(Fnv32_t hval) {return _fold(18,hval);}
+Fnv32_t fold17(Fnv32_t hval) {return _fold(17,hval);}
+Fnv32_t fold16(Fnv32_t hval) {return _fold(16,hval);}
+Fnv32_t fold15(Fnv32_t hval) {return _fold(15,hval);}
+Fnv32_t fold14(Fnv32_t hval) {return _fold(14,hval);}
+Fnv32_t fold13(Fnv32_t hval) {return _fold(13,hval);}
+Fnv32_t fold12(Fnv32_t hval) {return _fold(12,hval);}
+Fnv32_t fold11(Fnv32_t hval) {return _fold(11,hval);}
+Fnv32_t fold10(Fnv32_t hval) {return _fold(10,hval);}
+Fnv32_t fold9(Fnv32_t hval) {return _fold(9,hval);}
+Fnv32_t fold8(Fnv32_t hval) {return _fold(8,hval);}
+Fnv32_t fold7(Fnv32_t hval) {return _fold(7,hval);}
+Fnv32_t fold6(Fnv32_t hval) {return _fold(6,hval);}
+Fnv32_t fold5(Fnv32_t hval) {return _fold(5,hval);}
+Fnv32_t fold4(Fnv32_t hval) {return _fold(4,hval);}
+Fnv32_t fold3(Fnv32_t hval) {return _fold(3,hval);}
+Fnv32_t fold2(Fnv32_t hval) {return _fold(2,hval);}
+Fnv32_t fold1(Fnv32_t hval) {return _fold(1,hval);}
 
 typedef enum {
     FNV_UNDEF,
@@ -59,7 +81,36 @@ typedef enum {
 typedef enum {
     FOLD_UNDEF,
     FOLD_NOOP,
+    FOLD31,
+    FOLD30,
+    FOLD29,
+    FOLD28,
+    FOLD27,
+    FOLD26,
+    FOLD25,
+    FOLD24,
+    FOLD23,
+    FOLD22,
+    FOLD21,
+    FOLD20,
+    FOLD19,
+    FOLD18,
+    FOLD17,
     FOLD16,
+    FOLD15,
+    FOLD14,
+    FOLD13,
+    FOLD12,
+    FOLD11,
+    FOLD10,
+    FOLD9,
+    FOLD8,
+    FOLD7,
+    FOLD6,
+    FOLD5,
+    FOLD4,
+    FOLD3,
+    FOLD2,
     FOLD1,
 } FoldTypes;
 
@@ -80,7 +131,36 @@ static const struct {
 }  fold_types [] = {
         {FOLD_UNDEF, "undef", NULL},
         {FOLD_NOOP, "fold_noop", &fold_noop},
+        {FOLD31, "fold31", &fold31},
+        {FOLD30, "fold30", &fold30},
+        {FOLD29, "fold29", &fold29},
+        {FOLD28, "fold28", &fold28},
+        {FOLD27, "fold27", &fold27},
+        {FOLD26, "fold26", &fold26},
+        {FOLD25, "fold25", &fold25},
+        {FOLD24, "fold24", &fold24},
+        {FOLD23, "fold23", &fold23},
+        {FOLD22, "fold22", &fold22},
+        {FOLD21, "fold21", &fold21},
+        {FOLD20, "fold20", &fold20},
+        {FOLD19, "fold19", &fold19},
+        {FOLD18, "fold18", &fold18},
+        {FOLD17, "fold17", &fold17},
         {FOLD16, "fold16", &fold16},
+        {FOLD15, "fold15", &fold15},
+        {FOLD14, "fold14", &fold14},
+        {FOLD13, "fold13", &fold13},
+        {FOLD12, "fold12", &fold12},
+        {FOLD11, "fold11", &fold11},
+        {FOLD10, "fold10", &fold10},
+        {FOLD9, "fold9", &fold9},
+        {FOLD8, "fold8", &fold8},
+        {FOLD7, "fold7", &fold7},
+        {FOLD6, "fold6", &fold6},
+        {FOLD5, "fold5", &fold5},
+        {FOLD4, "fold4", &fold4},
+        {FOLD3, "fold3", &fold3},
+        {FOLD2, "fold2", &fold2},
         {FOLD1, "fold1", &fold1},
 };
 
