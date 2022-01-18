350b30b3b4abce295bc8e5746a74aeeef8ce8757
Felix Wischke (65278) <felix@adjust.com>
2022-01-18 12:08:53 +0100

    src/schaufel.h: initial commit of portability shim

---
+++ src/schaufel.h
@@ -0,0 +1,13 @@
+#ifndef _SCHAUFEL_H_
+#define _SCHAUFEL_H_
+
+#ifdef HAVE_CONFIG_H
+#include "schaufel_config.h"
+#define _SCHAUFEL_VERSION PACKAGE_VERSION
+#endif
+
+#ifndef _SCHAUFEL_VERSION
+#define _SCHAUFEL_VERSION "unknown unknowns"
+#endif
+
+#endif
