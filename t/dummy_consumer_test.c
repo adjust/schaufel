eebbe6ef2ff12e7c79e8e0fbf287e1a324860628
Felix Wischke (65278) <felix@adjust.com>
2022-02-01 11:33:33 +0100

    t/dummy_consumer_test.c: fix indentation and error path

--- t/dummy_consumer_test.c
+++ t/dummy_consumer_test.c
@@ -18,12 +18,23 @@ main(void)
     consumer_consume(c, msg);
     char *string =(char *) message_get_data(msg);
     pretty_assert(string != NULL);
-    if (string != NULL)
+    if (string != NULL) {
         pretty_assert(strncmp(string, "{\"type\":\"dummy\"}", 16) == 0);
         printf("%s\n", string);
+    } else
+        goto error;
     message_free(&msg);
     free(string);
     consumer_free(&c);
     config_destroy(&conf_root);
+
     return 0;
+
+    error:
+    message_free(&msg);
+    free(string);
+    consumer_free(&c);
+    config_destroy(&conf_root);
+
+    return 1;
 }
